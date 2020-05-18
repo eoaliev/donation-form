d0a85cd0e0fbf2f8de4bc8a916e30f2d2d3d3627
Emil Aliev <eo.aliev@bizprofi.ru>
2020-05-18 17:36:40 +0300

    Fix typo

--- client/src/store/Donation.js
+++ client/src/store/Donation.js
@@ -11,8 +11,8 @@ export default {
     currencies: [
       {name: "US Dollar", code: "USD", symbol: "$", rate: 1},
       {name: "Euro", code: "EUR", symbol: "€", rate: 0.897597},
-      {name: "British Pound", code: "GBP", symbol: "£", rate: 0.81755}
-      {name: "Russian Ruble", code: "RUB", symbol: "₽", rate: 63.461993}
+      {name: "British Pound", code: "GBP", symbol: "£", rate: 0.81755},
+      {name: "Russian Ruble", code: "RUB", symbol: "₽", rate: 63.461993},
     ],
     suggestion: 40,
     currency: 'USD',
